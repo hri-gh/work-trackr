988261c4fb86c19e4f67b1534f00f0e813e9217b
Hri <hri.gh@outlook.com>
2026-01-01 15:21:10 +0530

    feat(layout): integrate Navbar component into global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/layout/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="relative flex min-h-screen flex-col">
+          <Navbar />
+          <main className="flex-1 container mx-auto px-4 py-6">{children}</main>
+        </div>
       </body>
     </html>
   );
