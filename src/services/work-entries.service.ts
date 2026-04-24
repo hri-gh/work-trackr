3f5095c393173e9d0dc31907908ff82cabfd6eb0
Hri <hri.gh@outlook.com>
2026-04-24 11:14:51 +0530

    feat(services): add dashboard and worker services with statistics and entry management functions

---
+++ src/services/work-entries.service.ts
@@ -0,0 +1,80 @@
+import { prisma } from "@/lib/db/prisma";
+import { AuditContext } from "@/lib/audit/get-audit-context";
+import { logEvent } from "@/lib/audit/audit-log";
+import { AuditAction, AuditEntity } from "@/lib/audit/audit-types";
+import { CreateWorkEntryInput } from "@/schemas/work-entry.schema";
+
+
+// create a new work entry
+export const createWorkEntry = async (data: CreateWorkEntryInput, context: AuditContext) => {
+    const { date, amount, grainKg, note, workerIds } = data;
+
+    console.log("Parsed data:", date);
+    await prisma.$transaction(
+        workerIds.map((workerId) =>
+            prisma.workEntry.create({
+                data: {
+                    workerId,
+                    date: new Date(date),
+                    amount,
+                    grainKg,
+                    note,
+                },
+            })
+        )
+    );
+    // Log the event
+    await Promise.all(
+        workerIds.map((workerId) =>
+            logEvent({
+                action: AuditAction.CREATE_WORK_ENTRY,
+                entity: AuditEntity.WORK_ENTRY,
+                entityId: workerId,
+                newValue: { ...data },
+                ...context,
+            })
+        )
+    );
+}
+
+// mark entries as paid
+export const markEntriesPaid = async (entryIds: string[], context: AuditContext, paymentDate: Date) => {
+    const entries = await prisma.workEntry.findMany({
+        where: { id: { in: entryIds } },
+    });
+
+    await prisma.workEntry.updateMany({
+        where: { id: { in: entryIds } },
+        data: {
+            paid: true,
+            paymentDate: paymentDate
+        },
+    });
+
+    // log the event of data update
+
+    // for (const entry of entries) {
+    //     await logEvent({
+    //         action: AuditAction.MARK_PAID,
+    //         entity: AuditEntity.WORK_ENTRY,
+    //         entityId: entry.id,
+    //         oldValue: entry,
+    //         newValue: { ...entry, paid: true },
+    //         ...context
+    //     })
+
+    await Promise.all(
+        entries.map((entry) =>
+            logEvent({
+                action: AuditAction.MARK_PAID,
+                entity: AuditEntity.WORK_ENTRY,
+                entityId: entry.id,
+                oldValue: entry,
+                newValue: { ...entry, paid: true },
+                ...context,
+            })
+        )
+    );
+
+    return { count: entryIds.length };
+}
